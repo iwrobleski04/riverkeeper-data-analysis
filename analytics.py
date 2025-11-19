25821de5c58687fb15ccdd62a09312ac6f27ea33
Isabella Wrobleski <imw2298@nyu.edu>
2025-11-18 22:34:04 -0500

    places stats

--- analytics.py
+++ analytics.py
@@ -42,24 +42,35 @@ def run():
     )
 
     if page == "Basic Statistics":
-        st.markdown("<h3 style='text-align: center;'>Basic Statistics</h3>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center;'>Basic Statistics</h2>", unsafe_allow_html=True)
         # KPIs, summary tables
 
     elif page == "Active/Inactive Donors":
-        st.markdown("<h3 style='text-align: center;'>Active/Inactive Donors</h3>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center;'>Active/Inactive Donors</h2>", unsafe_allow_html=True)
         # Charts...
 
     elif page == "Top Donors":
-        st.markdown("<h3 style='text-align: center;'>Top Donors</h3>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center;'>Top Donors</h2>", unsafe_allow_html=True)
         # Leaderboards...
 
     elif page == "Frequent Donors":
-        st.markdown("<h3 style='text-align: center;'>Frequent Donors</h3>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center;'>Frequent Donors</h2>", unsafe_allow_html=True)
         # Histograms...
 
     elif page == "Donors by Location":
-        st.markdown("<h3 style='text-align: center;'>Donors by City and State</h3>", unsafe_allow_html=True)
-        # Maps, regional charts...
+        st.markdown("<h2 style='text-align: center;'>Donors by State and City</h2>", unsafe_allow_html=True)
+        st.space(size="medium")
+
+        st.markdown("<h4 style='text-align: center;'>Donors by State</h4>", unsafe_allow_html=True)
+        st.markdown("<ul style='text-align: center;'><li> Number of unique cities donated from</li><li>Number of donors</li><li>Total donations</li><li>Donations in the past 18 Months</li></ul>", unsafe_allow_html=True)
+        states = stats_by_state(data)
+        st.dataframe(states)
+
+        st.space(size="small")
+        st.markdown("<h4 style='text-align: center;'>Donors by Month</h4>", unsafe_allow_html=True)
+        st.markdown("<p style='text-align: center;'>Months and the number of donors whose last donation was in that month.</p>", unsafe_allow_html=True)
+        cities = stats_by_city(data)
+        st.dataframe(cities)
 
     elif page == "Donors by Time":
         st.markdown("<h2 style='text-align: center;'>Donors by Month and Year</h2>", unsafe_allow_html=True)
