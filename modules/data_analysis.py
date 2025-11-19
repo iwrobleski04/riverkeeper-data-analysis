f9f7417bccd4ff8b9b6d291564dcaa64d8351f1e
Isabella Wrobleski <imw2298@nyu.edu>
2025-11-18 23:10:53 -0500

    fix function

--- modules/data_analysis.py
+++ modules/data_analysis.py
@@ -158,7 +158,7 @@ def stats_by_state(df: pd.DataFrame) -> pd.DataFrame:
         most recent donation date
     '''
     # copy data and drop rows where there is no state
-    df["State"] = df["State"].replace("", pd.NA).fillna()
+    df["State"] = df["State"].replace("", np.nan)
     data = df.copy().dropna(subset=["State"])
 
     # group by city and create dataframe
