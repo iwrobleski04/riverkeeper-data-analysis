89885911eb7cecffbc38943eea91ec7712a54748
Isabella Wrobleski <imw2298@nyu.edu>
2025-11-18 23:15:26 -0500

    Update data_analysis.py

--- modules/data_analysis.py
+++ modules/data_analysis.py
@@ -164,6 +164,7 @@ def stats_by_state(df: pd.DataFrame) -> pd.DataFrame:
     # group by city and create dataframe
     g = data.groupby("State")
     res = pd.DataFrame({
+        "Cities" : g["City"].nunique(),
         "Donors" : g["Account ID"].nunique(),
         "Total Gifts (All Time)" : g["Total Gifts (All Time)"].sum(),
         "Number of Gifts Past 18 Months" : g["Number of Gifts Past 18 Months"].sum().astype(int)
@@ -195,7 +196,6 @@ def stats_by_city(df: pd.DataFrame) -> pd.DataFrame:
     # group by city and state and create dataframe
     g = data.groupby(["City", "State"])
     res = pd.DataFrame({
-        "Cities" : g["City"].nunique(),
         "Donors" : g["Account ID"].nunique(),
         "Total Gifts (All Time)" : g["Total Gifts (All Time)"].sum(),
         "Number of Gifts Past 18 Months" : g["Number of Gifts Past 18 Months"].sum().astype(int)
