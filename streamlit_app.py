6a5f62bfc306f02e1649fc324eb15a69afc2b597
Isabella Wrobleski <imw2298@nyu.edu>
2025-11-18 21:48:38 -0500

    nav

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit as st
 from dataset_merger import run as run_dataset_merger
 from analytics import run as run_analytics
 
-dataset_merger = st.Page(run_dataset_merger, title="Dataset Merger", icon="📁", default=True, pathname="dataset_merger.py")
-analytics = st.Page(run_analytics, title="Donor Analytics", icon="📊", pathname="analytics.py")
+dataset_merger = st.Page(run_dataset_merger, title="Dataset Merger", icon="📁", url_path="dataset_merger.py", default=True)
+analytics = st.Page(run_analytics, title="Donor Analytics", icon="📊", url_path="analytics.py")
 
 current_page = st.navigation([dataset_merger, analytics], position="top")
 current_page.run()
