56608c970fe600886a631ef3ebcbd06ea5cea66a
Isabella Wrobleski <imw2298@nyu.edu>
2025-11-19 18:51:53 -0500

    update bar charts

--- analytics.py
+++ analytics.py
@@ -134,12 +134,10 @@ def run():
         st.space(size="medium")
 
         st.markdown("<h4 style='text-align: center;'>Donors by Year</h4>", unsafe_allow_html=True)
-        st.markdown("<p style='text-align: center;'>Years and the number of donors whose last donation was in that year.</p>", unsafe_allow_html=True)
         yearly = stats_by_year(data)
-        st.bar_chart(yearly, color="#007633")
+        st.bar_chart(yearly, x_label="Year", y_label="Number of Donors whose Last Donation was in the Year", color="#007633")
 
         st.space(size="small")
         st.markdown("<h4 style='text-align: center;'>Donors by Month</h4>", unsafe_allow_html=True)
-        st.markdown("<p style='text-align: center;'>Months and the number of donors whose last donation was in that month.</p>", unsafe_allow_html=True)
         monthly = stats_by_month(data)
-        st.bar_chart(monthly, color="#007633", sort=False)
+        st.bar_chart(monthly, x_label="Month", y_label="Number of Donors whose Last Donation was in the Month", color="#007633", sort=False)
