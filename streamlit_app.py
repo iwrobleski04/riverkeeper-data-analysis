27a954aa878931df93357ceb78290e7193a1fe37
Isabella Wrobleski <imw2298@nyu.edu>
2025-11-18 21:35:10 -0500

    edit position

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 dataset_merger = st.Page("dataset_merger.py", title="Dataset Merger", icon="📁", default=True)
-analytics = st.Page("analytics.py", title="Donor Analytics", icon="📈")
+analytics = st.Page("analytics.py", title="Donor Analytics", icon="📊")
 
-current_page = st.navigation([dataset_merger, analytics], position="sidebar")
+current_page = st.navigation([dataset_merger, analytics], position="top")
 current_page.run()
