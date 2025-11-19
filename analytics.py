3540f74367ec4c0ceae90de872c0d2653f48fddf
Isabella Wrobleski <imw2298@nyu.edu>
2025-11-18 22:08:48 -0500

    month plot

--- analytics.py
+++ analytics.py
@@ -68,11 +68,12 @@ def run():
         st.text("")
         st.text("")
 
-        col1, col2 = st.columns([1, 1])
-        with col1:
-            st.markdown("<h5 style='text-align: center;'>Donors by Year</h5>", unsafe_allow_html=True)
-            st.markdown("<p style='text-align: center;'>Years and the number of donors whose last donation was in that year.</p>", unsafe_allow_html=True)
+        st.markdown("<h5 style='text-align: center;'>Donors by Year</h5>", unsafe_allow_html=True)
+        st.markdown("<p style='text-align: center;'>Years and the number of donors whose last donation was in that year.</p>", unsafe_allow_html=True)
+        yearly = stats_by_year(data)
+        st.bar_chart(yearly)
 
-
-            yearly = stats_by_year(data)
-            st.bar_chart(yearly)
+        st.markdown("<h5 style='text-align: center;'>Donors by Year</h5>", unsafe_allow_html=True)
+        st.markdown("<p style='text-align: center;'>Months and the number of donors whose last donation was in that month.</p>", unsafe_allow_html=True)
+        monthly = stats_by_month(data)
+        st.bar_chart(monthly)
