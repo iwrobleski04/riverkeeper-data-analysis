ad5900390d4a19c4845d425dde5f5ef21070e501
Isabella Wrobleski <imw2298@nyu.edu>
2025-11-18 21:41:41 -0500

    sidebar

--- analytics.py
+++ analytics.py
@@ -5,45 +5,40 @@ st.set_page_config(
     page_icon="📊",
 )
 
-st.title("Donor Analytics")
+def run():
+    st.markdown("<h1 style='text-align: center;'>Donor Analytics</h1>", unsafe_allow_html=True)
 
-page = st.sidebar.radio(
-    "Select an analysis",
-    [
-        "Basic Stats",
-        "Active Donors",
-        "Inactive Donors",
-        "Top Donors",
-        "Frequent Donors",
-        "By Location",
-        "By Time"
-    ]
-)
+    page = st.sidebar.radio(
+        [
+            "Basic Statistics",
+            "Active/Inactive Donors",
+            "Top Donors",
+            "Frequent Donors",
+            "Statistics by Location",
+            "Statistics by Time"
+        ]
+    )
 
-if page == "Basic Stats":
-    st.header("Basic Stats")
-    # KPIs, summary tables
+    if page == "Basic Statistics":
+        st.markdown("<h3 style='text-align: center;'>Basic Statistics</h3>", unsafe_allow_html=True)
+        # KPIs, summary tables
 
-elif page == "Active Donors":
-    st.header("Active Donors")
-    # Charts...
+    elif page == "Active/Inactive Donors":
+        st.markdown("<h3 style='text-align: center;'>Active/Inactive Donors</h3>", unsafe_allow_html=True)
+        # Charts...
 
-elif page == "Inactive Donors":
-    st.header("Inactive Donors")
-    # Analytics...
+    elif page == "Top Donors":
+        st.markdown("<h3 style='text-align: center;'>Top Donors</h3>", unsafe_allow_html=True)
+        # Leaderboards...
 
-elif page == "Top Donors":
-    st.header("Top Donors")
-    # Leaderboards...
+    elif page == "Frequent Donors":
+        st.markdown("<h3 style='text-align: center;'>Frequent Donors</h3>", unsafe_allow_html=True)
+        # Histograms...
 
-elif page == "Frequent Donors":
-    st.header("Frequent Donors")
-    # Histograms...
+    elif page == "Statistics by Location":
+        st.markdown("<h3 style='text-align: center;'>Statistics by City and State</h3>", unsafe_allow_html=True)
+        # Maps, regional charts...
 
-elif page == "By Location":
-    st.header("Donors by Location")
-    # Maps, regional charts...
-
-elif page == "By Time":
-    st.header("Donation Trends Over Time")
-    # line charts, seasonality
+    elif page == "Statistics by Time":
+        st.markdown("<h3 style='text-align: center;'>Statistics by Month and Year</h3>", unsafe_allow_html=True)
+        # line charts, seasonality
--- dataset_merger.py
+++ dataset_merger.py
@@ -11,63 +11,62 @@ st.set_page_config(
     page_icon="📁",
 )
 
-dataset_merger = st.Page("streamlit_app.py", title="Dataset Merger", icon="📊", default=True)
+def run():
+    
+    # connecting to google sheets 
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    creds_dict = json.loads(st.secrets["google_service_account"]["creds_json"])
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+    gc = gspread.authorize(creds)
 
+    # opening sheet
+    sheet_name = "Riverkeeper_Donors"
+    sh = gc.open(sheet_name)
+    worksheet = sh.sheet1
 
-# connecting to google sheets 
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds_dict = json.loads(st.secrets["google_service_account"]["creds_json"])
-creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
-gc = gspread.authorize(creds)
+    # load old data from sheet
+    data = worksheet.get_all_records()
+    old_df = pd.DataFrame(data)
 
-# opening sheet
-sheet_name = "Riverkeeper_Donors"
-sh = gc.open(sheet_name)
-worksheet = sh.sheet1
-
-# load old data from sheet
-data = worksheet.get_all_records()
-old_df = pd.DataFrame(data)
-
-st.markdown("<h1 style='text-align: center;'>Dataset Merger</h1>", unsafe_allow_html=True)
-st.markdown("<p style='text-align: center;'>Upload the new dataset below to merge with the existing dataset.</p>", unsafe_allow_html=True)
-st.text("")
-st.text("")
-
-# display existing data
-st.markdown("<p style='text-align: center;'>Existing Dataset:</p>", unsafe_allow_html=True)
-st.dataframe(old_df)
-
-# button to upload new csv
-uploaded_file = st.file_uploader("", type="csv")
-if uploaded_file:
-
-    # merge datasets
-    new_df = pd.read_csv(uploaded_file)
-    merged_df, new_rows_df = merge_csv(old_df, new_df, save=False)
-
-    # replace nans with empty strings to avoid error
-    merged_df_clean = merged_df.replace([np.inf, -np.inf], np.nan)
-    merged_df_clean = merged_df_clean = merged_df_clean.fillna('')
-
-    # update google sheet
-    worksheet.clear()
-    worksheet.update([merged_df_clean.columns.values.tolist()] + merged_df_clean.values.tolist())
-
-    st.success("Merged CSV saved and updated!")
+    st.markdown("<h1 style='text-align: center;'>Dataset Merger</h1>", unsafe_allow_html=True)
+    st.markdown("<p style='text-align: center;'>Upload the new dataset below to merge with the existing dataset.</p>", unsafe_allow_html=True)
     st.text("")
     st.text("")
 
-    # display new data
-    st.markdown("<p style='text-align: center;'>Merged Dataset:</p>", unsafe_allow_html=True)
-    st.dataframe(merged_df)
+    # display existing data
+    st.markdown("<p style='text-align: center;'>Existing Dataset:</p>", unsafe_allow_html=True)
+    st.dataframe(old_df)
 
-    csv = merged_df.to_csv(index=False).encode("utf-8")
-    new_csv = new_rows_df.to_csv(index=False).encode("utf-8")
+    # button to upload new csv
+    uploaded_file = st.file_uploader("", type="csv")
+    if uploaded_file:
 
-    # buttons to save merged data or new rows only
-    empty1, col1, col2, empty2 = st.columns([1, 2, 2, 1])
-    with col1:
-        st.download_button("Download Merged CSV", csv, "merged_data.csv", "text/csv")
-    with col2:
-        st.download_button("Download New Data CSV", new_csv, "new_data.csv", "text/csv")
+        # merge datasets
+        new_df = pd.read_csv(uploaded_file)
+        merged_df, new_rows_df = merge_csv(old_df, new_df, save=False)
+
+        # replace nans with empty strings to avoid error
+        merged_df_clean = merged_df.replace([np.inf, -np.inf], np.nan)
+        merged_df_clean = merged_df_clean = merged_df_clean.fillna('')
+
+        # update google sheet
+        worksheet.clear()
+        worksheet.update([merged_df_clean.columns.values.tolist()] + merged_df_clean.values.tolist())
+
+        st.success("Merged CSV saved and updated!")
+        st.text("")
+        st.text("")
+
+        # display new data
+        st.markdown("<p style='text-align: center;'>Merged Dataset:</p>", unsafe_allow_html=True)
+        st.dataframe(merged_df)
+
+        csv = merged_df.to_csv(index=False).encode("utf-8")
+        new_csv = new_rows_df.to_csv(index=False).encode("utf-8")
+
+        # buttons to save merged data or new rows only
+        empty1, col1, col2, empty2 = st.columns([1, 2, 2, 1])
+        with col1:
+            st.download_button("Download Merged CSV", csv, "merged_data.csv", "text/csv")
+        with col2:
+            st.download_button("Download New Data CSV", new_csv, "new_data.csv", "text/csv")
