b5aa3d32685d2cfdd339683836627e472106142f
Isabella Wrobleski <imw2298@nyu.edu>
2025-11-18 21:33:45 -0500

    fix paths

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-dataset_merger = st.Page("pages/dataset_merger.py", title="Dataset Merger", icon="📁", default=True)
-analytics = st.Page("pages/analytics.py", title="Donor Analytics", icon="📈")
+dataset_merger = st.Page("dataset_merger.py", title="Dataset Merger", icon="📁", default=True)
+analytics = st.Page("analytics.py", title="Donor Analytics", icon="📈")
 
 current_page = st.navigation([dataset_merger, analytics], position="sidebar")
 current_page.run()
